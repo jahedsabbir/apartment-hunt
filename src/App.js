d38f0971cc754325c0d02fff4c4dcf9daaf9880b
masud309060 <masud306090@gmail.com>
2020-11-15 21:01:50 +0600

    code added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <hi>This is app.js page</hi>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et distinctio qui mollitia illum quod, voluptate enim eveniet ea, deserunt quasi veritatis commodi laborum.</p>
+      <h3>change the code </h3>
     </div>
   );
 }
